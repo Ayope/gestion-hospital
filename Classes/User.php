eac04437352dc95ff87ed75c55ff09536dbf0f74
Abdellah EL GHOULAM <abdellahelghoulamdev@gmail.com>
2022-12-07 10:37:43 +0100

    ajouter User class

--- Classes/User.php
+++ Classes/User.php
@@ -1,73 +1,94 @@
 <?php
 include ("../config/db.php");
-class User {
-    //les attributes d'une session
-    protected $id;
-    protected $firstName;
-    protected $lastName;
-    protected $email;
-    protected $password;
-    protected $icon;
+class Session {
+	//les attributes d'une session
+	protected $id;
+	protected $firstName;
+	protected $lastName;
+	protected $email;
+	protected $password;
+	protected $icon;
     protected $role;
 
     //constructeur:
-    protected function __construct($firstName,$lastName,$email,$password,$icon,$role) {
-        $this->firstName= $firstName;
-        $this->lastName= $lastName;
-        $this->email=$email;
-        $this->password= $password;
-        $this->icon= $icon;
+	protected function __construct($firstName,$lastName,$email,$password,$icon,$role) {
+		$this->firstName= $firstName;
+		$this->lastName= $lastName;
+		$this->email=$email;
+		$this->password= $password;
+		$this->icon= $icon;
         $this->role= $role;
-    }
+	}
 
     //les getters et setters:
     protected function getId() {
-        return $this->id;
-    }
-    protected function setId($id) {
+		return $this->id;
+	}
+	protected function setId($id) {
        $this->id=$id;
      }
     protected function getFirstName() {
-        return $this->firstName;
-    }
-    protected function setFirstName($firstName) {
+		return $this->firstName;
+	}
+	protected function setFirstName($firstName) {
        $this->firstName=$firstName;
      }
     protected function getLastName() {
-        return $this->lastName;
-    }
-    protected function setLastName($lastName) {
+		return $this->lastName;
+	}
+	protected function setLastName($lastName) {
        $this->lastName=$lastName;
      }
     protected function getEmail() {
-        return $this->email;
-    }
-    protected function setEmail($email) {
+		return $this->email;
+	}
+	protected function setEmail($email) {
        $this->email=$email;
 
      }
-    protected function gatPassword() {
-        return $this->password;
-    }
-    protected function setPassword($password) {
+	protected function gatPassword() {
+		return $this->password;
+	}
+	protected function setPassword($password) {
        $this->password=$password;
 
      }
     protected function gatIcon() {
-        return $this->icon;
-    }
-    protected function setIcon($icon) {
+		return $this->icon;
+	}
+	protected function setIcon($icon) {
        $this->icon=$icon;
 
      }
     protected function getRole() {
-        return $this->role;
-    }
-    protected function setRole($role) {
+		return $this->role;
+	}
+	protected function setRole($role) {
        $this->role=$role;
 
      }
    public function login($email,$password){
+    global $bdd;
+      $sql="SELECT email ,password  from user where email = $email";
+      $result = $bdd->query($sql);
+      $numRows = $result->num_rows;
+      if ($numRows > 0){
+        $row = $result->fetch_assoc();
+          // settign the values of the attributes
+          if($password == row['password']){
+          $_SESSION['USER_ID'];
+          $_SESSION['ROLE'];
+          }
+          
+          
+      
+      }else{
+        // alert email invalide
+      }
+
+      
+
+
 
    }
    public function logOut(){
