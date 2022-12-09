2ce53e9c5bad3b1016b25417eefa5b3ad8577c5b
Abdellah EL GHOULAM <abdellahelghoulamdev@gmail.com>
2022-12-09 10:09:04 +0100

    fixing a bug on header location

--- Classes/user/User.php
+++ Classes/user/User.php
@@ -89,7 +89,7 @@ class User{
         $_SESSION['ID']= $row['id'];
         $_SESSION['ROLE']= $row['role'];
 
-          if($_SESSION['ROLE']=="admin"){
+        if($_SESSION['ROLE']=="admin"){
           header('location: ../Pages/dashboard-admin/Dashboard.php');
         }elseif($_SESSION['ROLE']=="doc"){
           header('location: ../Pages/Dashboard-doctor/dashboard.php');
