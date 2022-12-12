28c09a3562ee0d325be0eb69df1b31ccf9f2b44b
moussafia <moussafia.mohammed1999@gmail.com>
2022-12-12 21:42:34 +0100

    CR for CRUD appointment fixed and tested

---
+++ Classes/appointment/appointment.js
@@ -0,0 +1,4 @@
+function remplirFormAppointment(idForm){
+    
+
+}
--- Classes/appointment/test.php
+++ Classes/appointment/test.php
@@ -1,11 +1,15 @@
 <?php
-  
-    include "/Controllers/script.php";
-    
+    // include 'Classes\dbcon1.php';
+    // include "./Controllers/script.php";
+
+    // $database = new disconnect();
+    // $db = $database->connect_pdo();                                            
 ?>
 
+<?php include "scriptappointment.php"; ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -23,24 +27,45 @@
 
 <body>
     <form class="container-fluid" method="post" action="">
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">description</label>
-            <input type="text" class="form-control" id="dateDebut" name="description">
-        </div>
+      <input type="hidden" id="idHide" name="idHide">
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">date</label>
-            <input type="date" class="form-control" id="dateFin" name="date">
+            <input type="date" class="form-control" id="date" name="date">
         </div>
-        <button type="submit" class="btn btn-primary" name="save_apoointment">Submit</button>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">description</label>
+            <input type="text" class="form-control" id="description" name="description">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">code patient</label>
+            <input type="number" class="form-control" id="cp" name="cp">
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">code session</label>
+            <input type="number" class="form-control" name="cs" id="cs">
+        </div>
+        <button type="submit" class="btn btn-primary" name="save_Appointment">Submit</button>
     </form>
-
+    <div>
+        <table class=" table">
+            <th>id</th>
+            <th>description</th>
+            <th>dateA</th>
+            <th>code Patient</th>
+            <th>code session</th>
+            <th class="text-center">event</th>
+            <?php getAppointment(); ?>
+        </table>
+    </div>
+    <?php  ?>
+    <?php  ?>
 
     <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
     <!-- BS JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
     </script>
-    <script src="./Assests/js/Session.js"></script>
+    <script src="./Session.js"></script>
 </body>
 
 </html>
